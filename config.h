59c9127d8c9b993fa3b59e9b3ee7779a4890d251
rikku <rikku.genius@gmail.com>
2022-12-11 15:36:45 +0530

    changed tag icons

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
@@ -16,25 +16,34 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_red[]         = "#ff5370";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_red, col_cyan,  col_red  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "爵", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class               instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "st-256color",       NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Alacritty",         NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "VSCodium",          NULL,     NULL,           1 << 1,    0,          0,           0,        -1 },
+	{ "QtCreator",         NULL,     NULL,           1 << 1,    0,          0,           0,        -1 },
+	{ "Firefox",           NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
+	{ "Chromium",          NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
+	{ "TelegramDesktop",   NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
+	{ "discord",           NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
+	{ "Steam",             NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
+	{ "Lutris",            NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
+	{ "Gimp",              NULL,     NULL,           1 << 4,    1,          0,           0,        -1 },
+	{ NULL,                NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
